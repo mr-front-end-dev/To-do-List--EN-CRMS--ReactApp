8fcd1ce7b09e62247d77a5eec211ec116a05bed2
MYKYTA RYSIEV <nickpolska21@gmail.com>
2021-06-30 06:43:50 +0200

    Added saving tasks in localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Projects from "./Projects";
@@ -7,12 +7,21 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
+const getInitialTasks = () => {
+  const tasksFromLocalStorage = localStorage.getItem("tasks");
+  return tasksFromLocalStorage
+      ? JSON.parse(tasksFromLocalStorage)
+      : []
+  };
+
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "Switch to React.js", done: true },
-    { id: 2, content: "Have lunch", done: true },
-  ]);
+
+  const [tasks, setTasks] = useState(getInitialTasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone(!hideDone);
