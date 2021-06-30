bb584b9534797052e122f9fc4505c8310fe4d3e0
MYKYTA RYSIEV <nickpolska21@gmail.com>
2021-06-30 12:36:08 +0200

    Added colors and breakpoints

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    white: "#fff",
+    black: "#000",
+    deepskyblue: "#00bfff",
+    crimson: "#ec365b",
+    lightSlateGrey: "#778899",
+    lightGrey: "#d3d3d3",
+  },
+  breakpoint: {
+    mobileMax360: 360,
+    mobileMax414: 414,
+  },
+};
