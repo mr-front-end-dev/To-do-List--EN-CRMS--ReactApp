11f1388b62e0ee8a88d99ea8dee9e9b98ea30d03
MYKYTA RYSIEV <nickpolska21@gmail.com>
2022-01-19 19:53:35 +0100

    function for example tasks added

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Wrapper, Button } from './styled';
+import { 
+  toggleHideDone, 
+  setAllDone, 
+  selectAreTasksEmpty, 
+  selectHideDone, 
+  selectIsEveryTaskDone, 
+  fetchExampleTasks 
+} from '../tasksSlice';
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  <Wrapper>
-    {tasks.length > 0 && (
-      <>
-        <Button onClick={toggleHideDone}>
-          {hideDone ? "Show" : "Hide"} completed tasks
-        </Button>
-        <Button 
-          onClick={setAllDone}
-          disabled={tasks.every(({ done }) => done)}
-        >
-          Complete all tasks
-        </Button>
-      </>
-    )}
-  </Wrapper> 
-);
+const Buttons = () => {
+  const areTasksEmpty = useSelector(selectAreTasksEmpty);
+  const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
+  const hideDone = useSelector(selectHideDone);
+
+  const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      <Button onClick={() => dispatch(fetchExampleTasks())}>
+        Download sample tasks
+      </Button>
+      {!areTasksEmpty && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Show" : "Hide"} completed tasks
+          </Button>
+          <Button 
+            onClick={() => dispatch(setAllDone())}
+            disabled={isEveryTaskDone}
+          >
+            Complete all tasks
+          </Button>
+        </>
+      )}
+    </Wrapper> 
+  );
+};
 
 export default Buttons;
