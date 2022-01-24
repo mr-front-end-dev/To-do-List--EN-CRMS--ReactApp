f0ec4a1eeee9b591ccd39158998afb52cbb9a59d
MYKYTA RYSIEV <nickpolska21@gmail.com>
2022-01-24 14:26:30 +0100

    baby icon added for text

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -11,7 +11,7 @@ export default () => (
         body={
           <p contentEditable>
             <p>you can edit the text below</p>
-            <p>Hej! My name a Nick, and I am a beginner Junior Front End Developer and Junior Manual Software Tester.
+            <p>Hej! My name a Nick, and I am a beginner Junior Front-End (React) Developer and 👶 Junior Manual Software Tester with introduction to automation.
             My native language is Russian, but I learn and practice more English language, Polish and some German.</p> 
             <p>Now I try to study software testing and some coding of simple web applications.</p>
             <p>I want to know coding and speaking English very well to be useful in the labor market. 
